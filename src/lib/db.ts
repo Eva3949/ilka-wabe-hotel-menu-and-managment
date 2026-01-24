e934bd8a5b3eb8c8b782c69fd8d5140dd6707451
Eva3949 <samueleva3949@gmail.com>
2026-01-25 01:23:35 +0300

    ok

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "../db/schema";
 import 'dotenv/config';
-
+import "server-only";
 const connectionString = process.env.DATABASE_URL!;
 
 // For XAMPP MySQL, sometimes we need to be explicit with the connection config
